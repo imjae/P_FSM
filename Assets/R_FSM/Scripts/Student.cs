f7c24fa0147b464c389f2553d43a9e04e4540ae2
imjae <code@kakao.com>
2022-03-23 11:11:06 +0900

    ChangeState() 함수 구현 버전

--- Assets/R_FSM/Scripts/Student.cs
+++ Assets/R_FSM/Scripts/Student.cs
@@ -51,9 +51,10 @@ public class Student : BaseGameEntity
         // Student가 가질 수 있는 상태 개수 만큼 메모리 할당, 각 상태에 클래스 메모리 할당
         states = new State[System.Enum.GetValues(typeof(StudentStates)).Length];
         states[(int)StudentStates.RestAndSleep] = new StudentOwnedStates.RestAndSleep();
+        states[(int)StudentStates.StudyHard] = new StudentOwnedStates.StudyHard();
 
         // 현재 상태를 집에서 쉬는 "RestAndSleep" 상태로 설정
-        currentState = states[(int)StudentStates.RestAndSleep];
+        ChangeState(StudentStates.RestAndSleep);
 
         knowledge = 0;
         stress = 0;
@@ -67,10 +68,25 @@ public class Student : BaseGameEntity
     public override void Updated()
     {
         // PrintText("대기중입니다...");
-        if(currentState != null)
+        if (currentState != null)
         {
             currentState.Execute(this);
         }
     }
 
+    public void ChangeState(StudentStates newState)
+    {
+        // 새로 바꾸려는 상태가 비어있으면 상태를 바꾸지 않는다.
+        if (states[(int)newState] == null) return;
+
+        // 현재 재생중인 상태가 있으면 Exit() 메소드 호출
+        if (currentState != null)
+        {
+            currentState.Exit(this);
+        }
+
+        // 새로운 상태로 변경하고, 새로 바뀐 상태의 Enter() 메소드 호출
+        currentState = states[(int)newState];
+        currentState.Enter(this);
+    }
 }
--- Assets/R_FSM/Scripts/StudentOwnedStates.cs
+++ Assets/R_FSM/Scripts/StudentOwnedStates.cs
@@ -16,27 +16,77 @@ namespace StudentOwnedStates
             entity.PrintText("참대에 누워 잠을 잔다.");
         }
         public override void Execute(Student entity)
-		{
-			entity.PrintText("zzZ~ zz~ zzzzZ~~");
+        {
+            entity.PrintText("zzZ~ zz~ zzzzZ~~");
 
-			// 피로가 0이 아니면
-			if ( entity.Fatigue > 0 )
-			{
-				// 피로 10씩 감소
-				entity.Fatigue -= 10;
-			}
-			// 피로가 0이면
-			else
-			{
-				// 도서관에 가서 공부를 하는 "StudyHard" 상태로 변경
-				// entity.ChangeState(StudentStates.StudyHard);
-			}
-		}
+            // 피로가 0이 아니면
+            if (entity.Fatigue > 0)
+            {
+                // 피로 10씩 감소
+                entity.Fatigue -= 10;
+            }
+            // 피로가 0이면
+            else
+            {
+                // 도서관에 가서 공부를 하는 "StudyHard" 상태로 변경
+                entity.ChangeState(StudentStates.StudyHard);
+            }
+        }
 
-		public override void Exit(Student entity)
-		{
-			entity.PrintText("침대를 정리하고 집 밖으로 나간다.");
-		}
+        public override void Exit(Student entity)
+        {
+            entity.PrintText("침대를 정리하고 집 밖으로 나간다.");
+        }
     }
+    public class StudyHard : State
+    {
+        public override void Enter(Student entity)
+        {
+            entity.CurrentLocation = Locations.Library;
+
+            entity.PrintText("공부를 하기 위해 도서관에 왔다.. 힘차..게.. 공부를 하자");
+        }
+
+        public override void Execute(Student entity)
+        {
+            entity.PrintText("공부 공부 공부 공부...");
+
+            // 지식, 스트레스, 피로가 1씩 증가
+            entity.Knowledge++;
+            entity.Stress++;
+            entity.Fatigue++;
+
+            // 지식이 3~10 사이가 되면 
+            if (entity.Knowledge >= 3 && entity.Knowledge <= 10)
+            {
+                int isExit = Random.Range(0, 2);
+                if (isExit == 1 || entity.Knowledge == 10)
+                {
+                    // 강의실에 가서 시험을 보는 "TakeAExam" 상태로 변경
+                    entity.ChangeState(StudentStates.TakeAExam);
+                }
+            }
+
+            // 스트레스가 20 이상이 되면
+            if (entity.Stress >= 20)
+            {
+                // PC방에 가서 게임을 하는 "PlayAGame" 상태로 변경
+                entity.ChangeState(StudentStates.PlayAGame);
+            }
+
+            // 피로가 50 이상이 되면
+            if (entity.Fatigue >= 50)
+            {
+                // 집에 가서 쉬는 "RestAndSleep" 상태로 변경
+                entity.ChangeState(StudentStates.RestAndSleep);
+            }
+        }
+
+        public override void Exit(Student entity)
+        {
+            entity.PrintText("자리를 정리하고 도서관을 나간다.");
+        }
+    }
+
 }
 
