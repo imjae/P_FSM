a2cd462ee86458f4aef83e7321b2445412d5dd09
imjae <code@kakao.com>
2022-03-31 11:55:32 +0900

    GameController 객체에 Unemployed 객체 관련 로직 추가

--- Assets/R_FSM/Scripts/GameController.cs
+++ Assets/R_FSM/Scripts/GameController.cs
@@ -6,10 +6,11 @@ public enum Locations { SweetHome = 0, Library, LectureRoom, PCRoom, Pub };
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField]
-    private string[] arrayStudents;
-    [SerializeField]
-    private GameObject studentPrefab;
+    [SerializeField] private string[] arrayStudents;
+    [SerializeField] private GameObject studentPrefab;
+    [SerializeField] private string[] arrayUnemployeds;
+    [SerializeField] private GameObject unemployedPrefab;
+
 
     // 재생 제어를 위한 모든 에이전트 리스트
     private List<BaseGameEntity> entityList;
@@ -26,6 +27,17 @@ public class GameController : MonoBehaviour
             GameObject clone = Instantiate(studentPrefab);
             Student entity = clone.GetComponent<Student>();
             entity.Setup(arrayStudents[i]);
+
+            // 에이전트들의 재생 제어를 위해 리스트에 저장
+            entityList.Add(entity);
+        }
+
+        for (int i = 0; i < arrayStudents.Length; i++)
+        {
+            // 에이전트 생성, 초기화 메소드 호출
+            GameObject clone = Instantiate(unemployedPrefab);
+            Unemployed entity = clone.GetComponent<Unemployed>();
+            entity.Setup(arrayUnemployeds[i]);
 
             // 에이전트들의 재생 제어를 위해 리스트에 저장
             entityList.Add(entity);
